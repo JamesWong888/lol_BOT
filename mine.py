9b994170a788df6a5dd3c1a88032994a9bd49996
MeHead <39626301+MeHead@users.noreply.github.com>
2018-06-22 18:18:37 +0100

    Update mine.py

--- mine.py
+++ mine.py
@@ -14,18 +14,26 @@ APIKEY = 'RGAPI-cece9a7b-953a-4d6c-89b9-6e8609135e91'
 
 @client.event # Displays the Eg. 'Playing League of Legends' message on discord.
 async def on_ready():
-    await client.change_presence(game=discord.Game(name =  ' running 24/7! test6'))
+    await client.change_presence(game=discord.Game(name =  ' running 24/7! test7'))
     print('Logged in as ' + client.user.name)
 
     
 @client.command()
 async def rotation():
+        
+    await client.say("Success!1")
     URL = "https://" + REGION + ".api.riotgames.com/lol/platform/v3/champions?freeToPlay=true&api_key=" + APIKEY
+        
+    await client.say("Success!2")
     response = requests.get(URL)
-    responseJSON = response.json()    
+        
+    await client.say("Success!3")
+    responseJSON = response.json()
+        
+    await client.say("Success!4")
     printQueue = []
     
-    await client.say("Success!")
+    await client.say("Success!5")
     
 
     
