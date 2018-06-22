0d4e024ad640debb2aa606ce667c08a1db6dbcda
MeHead <39626301+MeHead@users.noreply.github.com>
2018-06-22 18:05:37 +0100

    Update mine.py

--- mine.py
+++ mine.py
@@ -21,73 +21,33 @@ async def on_ready():
 async def testy():
     await client.say("hello")
 
-def requestRankedData(REGION, ID, APIKEY): # Returns RANKED with input: ID
-    URL = "https://" + REGION + ".api.riotgames.com/lol/league/v3/positions/by-summoner/" + ID + "?api_key=" + APIKEY
+@client.command(brief='Shows the free weekly champion rotation.')
+async def rotation():
+    URL = "https://" + REGION + ".api.riotgames.com/lol/platform/v3/champions?freeToPlay=true&api_key=" + APIKEY
     response = requests.get(URL)
-    return response.json()
-
-def summonerNameToID(summonerName): # Username to ID
-    responseJSON  = requestSummonerData(REGION, summonerName, APIKEY)
-    try:
-        return str(responseJSON['id'])
-    except KeyError:
-        return None
-
-def requestRank(summonerName): # Returns a string/array with ONE user rank info in 'pretty format' with input: Username
-    printQueue = {}
-    await client.say(summonerInfo)
-    ID = summonerNameToID(summonerName)
-    if (ID == None):
-        return None
-    responseJSON2 = requestRankedData(REGION, ID, APIKEY)
-
-    num = 3
-    for i in range(1):
-        try:
-            if (responseJSON2[0]["queueType"] == "RANKED_SOLO_5x5"):
-                num = 0
-            elif (responseJSON2[1]["queueType"] == "RANKED_SOLO_5x5"):
-                num = 1
-        except Exception:
-            continue
-        
-        finally:
-            if (num == 0 or num == 1):
-                summonerName = str(responseJSON2[num]['playerOrTeamName'])
-                rank = str.title(responseJSON2[num]['tier']) + " " + str(responseJSON2[num]['rank'])
-                winRate = str(responseJSON2[num]['wins']*100//(responseJSON2[num]['losses']+responseJSON2[num]['wins'])) + "%"
-                gamesPlayed = str(responseJSON2[num]['wins'] + responseJSON2[num]['losses']) + "G"
-                
-                printQueue['summonerName'] = summonerName
-                printQueue['rank'] = rank
-                printQueue['winRate'] = winRate
-                printQueue['gamesPlayed'] = gamesPlayed
-
-            elif (num == 3):
-                printQueue['summonerName'] = str(summonerName)
-                printQueue['rank'] = 'Unranked'
-                printQueue['winRate'] = "N/A"
-                printQueue['gamesPlayed'] = "N/A"
-                
-            else:
-                printQueue.append(str("Error Summoner: " + summonerName))        
-
-    return printQueue
-
-@client.command(brief = 'Shows the users solo queue rank.', pass_context = True)
-async def rank(ctx, summonerName):
+    responseJSON = response.json()    
+    printQueue = []
     
-    await client.say("Searching for rank")
+    await client.("Success!")
     
-    summonerInfo = requestRank(summonerName)
-    if (summonerInfo == None):
-        await client.say(ctx.message.author.mention + ", '" + summonerName + "' not found. Please check spelling")
-        return
-    
-    await client.say ("```" + summonerInfo['summonerName'] + "  " +
-                      summonerInfo['rank'] + "  " +
-                      summonerInfo['winRate'] + "  " +
-                      summonerInfo['gamesPlayed'] + "```")
+    for counter in range(14):
+        champID = responseJSON['champions'][counter]['id']
+        champName = _staticData.champDict['data'][str(champID)]['key']
+        champTitle = _staticData.champDict['data'][str(champID)]['title'] # Eg. Lux:   The Lady of Luminosity
+        printQueue.append('{:15}{:10}'.format(champName, champTitle))
+
+    await client.say("```This week's free rotation is: \n\n" +
+                     printQueue[0] + "\n" +
+                     printQueue[1] + "\n" +
+                     printQueue[2] + "\n" +
+                     printQueue[3] + "\n" +
+                     printQueue[4] + "\n" +
+                     printQueue[5] + "\n" +
+                     printQueue[6] + "\n" +
+                     printQueue[7] + "\n" +
+                     printQueue[8] + "\n" +
+                     printQueue[9] + "```")
+
 
 
 
