7ef535c5f1d9d8c4f0ce70145d2b9694e1c5adb0
MeHead <39626301+MeHead@users.noreply.github.com>
2018-06-22 15:56:06 +0100

    Update mine.py

--- mine.py
+++ mine.py
@@ -11,10 +11,13 @@ client = commands.Bot(command_prefix = '?')
 
 @client.event # Displays the Eg. 'Playing League of Legends' message on discord.
 async def on_ready():
-    await client.change_presence(game=discord.Game(name =  ' running 24/7! test'))
+    await client.change_presence(game=discord.Game(name =  ' running 24/7! test2'))
     print('Logged in as ' + client.user.name)
 
 
-           
+@client.command()
+async def testy():
+    await client.say("hello")
+    
 client.run(os.getenv('TOKEN'))
 
