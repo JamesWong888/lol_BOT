b0884725b3d72ad66f381bf032bae40ff0623f7b
MeHead <39626301+MeHead@users.noreply.github.com>
2018-06-22 16:06:57 +0100

    Update mine.py

--- mine.py
+++ mine.py
@@ -5,13 +5,14 @@ import asyncio
 import time
 import os
 import random
+import requests
 
 Client = discord.Client()
 client = commands.Bot(command_prefix = '?')
 
 @client.event # Displays the Eg. 'Playing League of Legends' message on discord.
 async def on_ready():
-    await client.change_presence(game=discord.Game(name =  ' running 24/7! test2'))
+    await client.change_presence(game=discord.Game(name =  ' running 24/7! test3'))
     print('Logged in as ' + client.user.name)
 
 
