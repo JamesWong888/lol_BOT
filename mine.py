0b62e6d43d8c536f9abc17df958bcc1660b9136a
MeHead <39626301+MeHead@users.noreply.github.com>
2018-06-23 09:30:34 +0100

    Update mine.py

--- mine.py
+++ mine.py
@@ -13,16 +13,16 @@ REGION = os.getenv('REGION')
 RIOTKEY = os.getenv('RIOTKEY')
 
 
-@client.event # Displays the Eg. 'Playing League of Legends' message on discord.
+@client.event
 async def on_ready():
     await client.change_presence(game=discord.Game(name =  ' now running 24/7!'))
     print('Logged in as ' + client.user.name)
 
+    
 @client.command()
 async def testy():
     await client.say("hello")
-    await client.say(REGION)
-    await client.say(RIOTKEY)
 
+    
 client.run(os.getenv('BOTTOKEN'))
 
