cef8b8e43fa17846bd80dfe8e78d07a17d80e019
MeHead <39626301+MeHead@users.noreply.github.com>
2018-06-25 18:27:22 +0100

    Update _main.py

--- _main.py
+++ _main.py
@@ -30,7 +30,7 @@ async def testy():
 @client.command()
 async def testyy():
     embed=discord.Embed(title="Level 95", description="Europe West")
-    embed.set_author(name="MeHead",, icon_url="http://ddragon.leagueoflegends.com/cdn/6.24.1/img/profileicon/23.png")
+    embed.set_author(name="MeHead", icon_url="http://ddragon.leagueoflegends.com/cdn/6.24.1/img/profileicon/23.png")
     embed.set_thumbnail(url="http://opgg-static.akamaized.net/images/medals/platinum_2.png")
     embed.add_field(name="Platinum 2", value="48%   231G", inline=True)
     embed.set_footer(text="Mastery: 381,032", icon_url="http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/Anivia.png")
